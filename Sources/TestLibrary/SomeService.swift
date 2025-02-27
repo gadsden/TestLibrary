b4d86da96ad8b47138fdfa171940f1687dd298f9
Me <jacob.waechter@gmail.com>
2025-02-27 11:08:11 -0800

    initial commit

---
+++ Sources/TestLibrary/SomeService.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  TestLibrary
+//
+//  Created by Jacob Waechter on 2/27/25.
+//
+
+import Foundation
+
+public class SomeService{
+    public static func greet() -> String{
+        return "Greetings!"
+    }
+}
